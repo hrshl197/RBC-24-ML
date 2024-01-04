3625a6b8e09ef0f6bf0609b62082c84871b77c5a
Pulin-Mori <pulin.mori24123@gmail.com>
2024-01-04 08:52:43 +0530

    new environmwnt xreated

---
+++ Pulin/Stage-3/Environment.py
@@ -0,0 +1,24 @@
+class siloEnvironment:
+    def __init__(self):
+        Silos_State=[["", "", ""],["", "", ""],["", "", ""],["", "", ""],["", "", ""]]
+
+    def get_state(self):
+        pass
+
+    def get_action(self):
+        return [0,1,2,3,4]
+
+    def take_action(self):
+        pass
+
+    def get_reward(self):
+        pass
+
+    def reset(self):
+        pass
+
+    def is_done(self):
+        pass
+
+    def switch_agent_turn(self):
+        pass
