6183d823c990b72eb6fb5baec765f2877d7e904d
Pulin-Mori <pulin.mori24123@gmail.com>
2024-01-09 23:24:46 +0530

    Final Day of Stage-3
    Found major flaws in Stage-3 file structure and there was loop created as Environment is calling for Epsilon_greedy instance and it is calling environment instance and the same is true for environment and agent

--- Pulin/Stage-3/Environment.py
+++ Pulin/Stage-3/Environment.py
@@ -9,6 +9,7 @@ class siloEnvironment:
         self.blue_won_silo=0
         self.total_balls=0
         self.reward=0
+        self.epsilon=0 # temporary completely wrong
         self.is_done=False
         
     def reset(self):
@@ -23,36 +24,36 @@ class siloEnvironment:
         # Perform an action and return the new state, reward, and whether the game is done
         # select agent either blue or red
         s = randint(0,1)
-        self.EG = eg()
+        self.EG = eg(self.epsilon)
         if s==0:
             print("Blue agent selected")
-            Silo_number=self.EG.choose_action(self.q_values)
+            self.Silo_number=self.EG.choose_action(self.q_values)
         elif s==1:
             print("Red agent selected")
-            Silo_number=self.EG.choose_action(self.q_values)
+            self.Silo_number=self.EG.choose_action(self.q_values)
         else:
             print("Error in agent selsction")
         
-        previous_Silo_State=self.Silo_State
+        self.previous_Silo_State=self.Silo_State
 
         # put the ball in silo 
-        if 0<=Silo_number<=4: # Checking wheather the selected silo number is in five
-            if len(self.Silo_State[Silo_number])<4: # checking wheather the silo is empty
-                for i in range(len(self.Silo_State[Silo_number])):
+        if 0<=self.Silo_number<=4: # Checking wheather the selected silo number is in five
+            if len(self.Silo_State[self.Silo_number])<4: # checking wheather the silo is empty
+                for i in range(len(self.Silo_State[self.Silo_number])):
                     if s==0:
-                        if self.Silo_State[Silo_number][i] == '':
-                            self.Silo_State[Silo_number][i] = 'o'
+                        if self.Silo_State[self.Silo_number][i] == '':
+                            self.Silo_State[self.Silo_number][i] = 'o'
                             break
                     elif s==1:
-                        if self.Silo_State[Silo_number][i] == '':
-                            self.Silo_State[Silo_number][i] = 'x'
+                        if self.Silo_State[self.Silo_number][i] == '':
+                            self.Silo_State[self.Silo_number][i] = 'x'
                             break
             else:
                 print("Silo is already filled")
         else:
             print("Error in Silo Number")
 
-        new_Silo_State=self.Silo_State
+        self.new_Silo_State=self.Silo_State
 
         # TODO Generate Reward here generated from reward file 
         # self.reward=reward_func(self,previous_Silo_Stat,new_Silo_State,self.Silo_number,s)
--- Pulin/Stage-3/agent.py
+++ Pulin/Stage-3/agent.py
@@ -4,27 +4,37 @@ from collections import deque
 from Epsilon_Greedy import EpsilonGreedy as eg
 from Environment import siloEnvironment as ev 
 import numpy as np
+from model import Linear_QNet, QTrainer
+from helper import plot
 
 MAX_MEMORY=100_000_000
+BATCH_SIZE=1000
+LR=0.001
 
 class RLAgent:
-    def __init__(self, state_space_size, action_space_size):
+    def __init__(self):
         # Initialize your agent's parameters and policy here
         self.n_games=0
         self.epsilon=0 # control the randomness
-        self.gamma=0 # discount rate
+        self.gamma=0.9 # discount rate
         self.memory=deque(maxlen=MAX_MEMORY) # pop from left is max memory get full 
-        self.model = None # waiting for model
-        self.trainer = None # waiting for model
+        self.model = Linear_QNet(11, 256, 3) 
+        self.trainer = QTrainer(self.model, lr=LR, gamma=self.gamma)
 
     def remember(self, state, action, reward, next_state, done):
-        pass
+        self.memory.append((state, action, reward, next_state, done))
 
     def train_long_memory(self):
-        pass
+        if len(self.memory) > BATCH_SIZE:
+            mini_sample = random.sample(self.memory, BATCH_SIZE) # list of tuples
+        else:
+            mini_sample = self.memory
+
+        states, actions, rewards, next_states, dones = zip(*mini_sample)
+        self.trainer.train_step(states, actions, rewards, next_states, dones)
 
     def train_short_memory(self, state, action, reward, next_state, done):
-        pass
+        self.trainer.train_step(state, action, reward, next_state, done)
 
     """def get_action(self, state):
         pass
@@ -42,17 +52,38 @@ def train():
     while True:
         game.step()
         # get old state
-        state_old = 
+        state_old = game.previous_Silo_State
 
         # get move
-        final_move =
+        final_move =game.Silo_number
 
         # perform move and get new state
-        state_new =
+        state_new =game.new_Silo_State
         
         # train short memory
+        agent.train_short_memory(state_old,final_move,game.reward,state_new,game.is_done)
 
         # remember
+        agent.remember(state_old,final_move,game.reward,state_new,game.is_done)
 
         if game.is_done:
             # train long memory, plot result
+            print('Final State : ',state_new)
+            game.reset()
+            agent.n_games+=1
+            agent.train_long_memory()
+            
+            if game.reward>record:
+                record=game.reward
+                agent.model.save() 
+
+            print('Game', agent.n_games, 'Score', game.reward, 'Record:', record)
+
+            plot_scores.append(game.reward)
+            total_score += game.reward
+            mean_score = total_score / agent.n_games
+            plot_mean_scores.append(mean_score)
+            plot(plot_scores, plot_mean_scores)
+
+if __name__=="__main__":
+    train()
