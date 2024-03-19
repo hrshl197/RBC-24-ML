ea7d523cc2b9e5de0915e3069b6749c2c881850f
Pulin-Mori <pulin.mori24123@gmail.com>
2024-03-19 20:27:16 +0530

    Update Generator.py

--- Pulin/Stage-9/Generator.py
+++ Pulin/Stage-9/Generator.py
@@ -1,6 +1,7 @@
 from Def_Off_2 import main as m
 import copy
 
+x=1
 state = [[],["","",""],["o","",""],["x","",""],["o","o",""],["o","x",""],["x","o",""],["x","x",""],["o","o","o"],["o","o","x"],["o","x","o"],["o","x","x"],["x","o","o"],["x","o","x"],["x","x","o"],["x","x","x"]]
 file_path = r'E:\RBC-24-ML\Pulin\Stage-9\use_output_1.txt'
 with open(file_path, 'r') as file:
@@ -19,5 +20,6 @@ with open(file_path, 'r') as file:
         #print("temp_list: ",temp_list)
         next_state=m(temp_list)
         #print(next_state)
-        print(use_list," ---> ",next_state)
+        print(x,".",use_list," ---> ",next_state)
+        x+=1
         
